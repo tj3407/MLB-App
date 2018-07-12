787b489f75212cf6dc1502e913a90bf4930ba94e
TJ Sornet <tsornet@gmail.com>
2018-07-11 19:48:56 -0700

    Deploying

--- server.js
+++ server.js
@@ -13,28 +13,25 @@ const parser = require('body-parser');
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 
-// Mysportsfeed
-var MySportsFeeds = require("mysportsfeeds-node");
-var msf = new MySportsFeeds("1.2", true);
-msf.authenticate("djxtremor", "tjdev456");
+// // Mysportsfeed
+// var MySportsFeeds = require("mysportsfeeds-node");
+// var msf = new MySportsFeeds("1.2", true);
+// msf.authenticate("djxtremor", "tjdev456");
 
-const leagueController = {
-  index: (request, response) => {
-    console.log('requesting api')
-    msf.getData('mlb', '2018-regular', 'overall_team_standings', 'json', {})
-      .then(league => response.json(league))
-      .catch(error => console.log(error))
-    // var data = msf.getData('mlb', '2018-regular', 'overall_team_standings', 'json', {});
-    // console.log(data);
-    // response.json(data);
-  }
-}
+// const leagueController = {
+//   index: (request, response) => {
+//     console.log('requesting api')
+//     msf.getData('mlb', '2018-regular', 'overall_team_standings', 'json', {})
+//       .then(league => response.json(league))
+//       .catch(error => console.log(error))
+//   }
+// }
 
-app
-  .get('/', leagueController.index)
-  .all('*', (req, res, next) => {
-  res.sendFile(path.resolve('dist/index.html'))
-});
+// app
+//   .get('/', leagueController.index)
+//   .all('*', (req, res, next) => {
+//   res.sendFile(path.resolve('dist/index.html'))
+// });
 
 
 
